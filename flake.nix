3995b910627fe799b2c07d7d1f4117ad2ef920f6
loockeeer <lulu.r1313@gmail.com>
2026-04-21 16:38:09 +0200

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Flake pour Programmation 2";
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs?ref=25.11";
+  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+      devShells.${system}.default =
+        let
+          pkgs = import nixpkgs { system = system; };
+          ocamlCtx = pkgs.ocaml-ng.ocamlPackages_5_1;
+        in
+        pkgs.mkShell {
+          packages = [
+            ocamlCtx.lsp
+            ocamlCtx.dune_3
+            ocamlCtx.ocaml
+            ocamlCtx.utop
+            ocamlCtx.fix
+            ocamlCtx.ocaml-lsp
+            ocamlCtx.menhir
+            ocamlCtx.menhirLib
+            ocamlCtx.zarith
+            ocamlCtx.ocamlformat
+          ];
+        };
+    };
+}
