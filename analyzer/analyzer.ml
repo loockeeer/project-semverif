d396996c6c27d55368a152a6b8d91919984c8317
loockeeer <lulu.r1313@gmail.com>
2026-05-29 21:23:03 +0200

    feat: add the analyzers to the CLI

--- analyzer/analyzer.ml
+++ analyzer/analyzer.ml
@@ -18,8 +18,38 @@ let doit filename =
   let cfg = Tree_to_cfg.prog prog in
   if !Options.verbose then Format.printf "%a" ControlFlowGraphPrinter.print_cfg cfg ;
   ControlFlowGraphPrinter.output_dot !Options.cfg_out cfg ;
-  Iterator.iterate cfg
-
+  let module PartialIterator = Iterator.Make (Domains.Domain.MakeForwardOnly) in
+  match !Options.domain with
+  | "sign" ->
+          let module SignIterator =
+              PartialIterator (Domains.Sign)
+          in
+          SignIterator.iterate cfg
+  | "interval" ->
+          let module IntervalIterator =
+              PartialIterator (Domains.Interval)
+          in
+          IntervalIterator.iterate cfg
+  | "congruence" ->
+          let module CongruenceIterator =
+              PartialIterator (Domains.Congruence)
+          in
+          CongruenceIterator.iterate cfg
+  | "reducedProduct" ->
+          let module ReducedProductIterator =
+              PartialIterator (Domains.ReducedProduct)
+          in
+          ReducedProductIterator.iterate cfg
+  | "polyhedral" ->
+          let module PolyhedralIterator = 
+              PartialIterator (Domains.Polyhedral)
+          in
+          PolyhedralIterator.iterate cfg
+  | s -> (
+        Printf.printf "error: the value domain %s does not exist\n" s;
+        print_endline "available value domains: sign, interval, congruence, reducedProduct, polyhedral";
+        exit 1
+        )
 (* parses arguments to get filename *)
 let main () =
   let _ = Options.init () in
