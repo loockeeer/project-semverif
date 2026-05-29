d5c022ac28d3df57cf7aaf75cde4d6f9a5ab5a4f
loockeeer <lulu.r1313@gmail.com>
2026-05-29 21:26:17 +0200

    refactor: remove boilerplate iterator code

--- iterator/iterator.ml
+++ iterator/iterator.ml
@@ -85,12 +85,3 @@ module Make (DM : Domain.DOMAIN_MAKE) (VD: ValueDomain.VALUE_DOMAIN) =
             let _ = iter_func main in
             ()
 end
-
-let iterate cfg =
-  let _ = Random.self_init () in
-  let iter_arc arc : unit = match arc.arc_inst with _ -> failwith "TODO" in
-  let iter_node node : unit = Format.printf "<%i>: ⊤@ " node.node_id in
-  List.iter iter_arc cfg.cfg_arcs ;
-  Format.printf "Node Values:@   @[<v 0>" ;
-  List.iter iter_node cfg.cfg_nodes ;
-  Format.printf "@]"
