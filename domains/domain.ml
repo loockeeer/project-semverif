6447a7f798e2a63d87271db9a690a64827446002
loockeeer <lulu.r1313@gmail.com>
2026-05-29 21:23:16 +0200

    feat: start implementing a domain functor

--- domains/domain.ml
+++ domains/domain.ml
@@ -18,7 +18,7 @@ end
   (for instance: a map from variable to their bounds).
  *)
 
-module type DOMAIN_MAKE = functor(_: ValueDomain.VALUE_DOMAIN) -> sig
+module type DOMAIN = sig
   (* type of abstract elements *)
   (* an element of type t abstracts a set of mappings from variables
        to integers
@@ -57,4 +57,37 @@ module type DOMAIN_MAKE = functor(_: ValueDomain.VALUE_DOMAIN) -> sig
 
   (* prints *)
   val pp : Format.formatter -> t -> unit
+end
+
+module type DOMAIN_MAKE = functor(_: ValueDomain.VALUE_DOMAIN) -> DOMAIN
+
+module MakeForwardOnly(Vd: ValueDomain.VALUE_DOMAIN) : DOMAIN = struct
+    type t = Vd.t VarMap.t
+
+    let init = VarMap.empty
+    let bottom = VarMap.empty
+    let is_bottom = VarMap.exists (fun _ -> Vd.is_bottom)
+
+    let rec get_value dom x =
+        match x with
+        | CFG_int_const i -> Vd.const i
+        | CFG_int_rand (from_c, to_c) -> Vd.rand from_c to_c
+        | CFG_int_unary (op, expr) -> Vd.unary (get_value dom expr) op
+        | CFG_int_binary (op, e1, e2) -> Vd.binary  (get_value dom e1)
+                                                    (get_value dom e2)
+                                                    op
+        | CFG_int_var(var) -> VarMap.find var dom
+
+    let assign dom var iexpr =
+        VarMap.add var (get_value dom iexpr) dom
+
+    let guard dom bexpr = failwith "wip"
+    let join d1 d2 = failwith "wip"
+    let meet d1 d2 = failwith "wip"
+    let widen d1 d2 = failwith "wip"
+
+    let leq d1 d2 = failwith "wip"
+    let pp fmt dom = failwith "wip"
+
+    let narrow = failwith "not implemented"
 end
