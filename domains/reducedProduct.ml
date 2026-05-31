581999f6d4807759c9108a6040e905b7964f8f66
BRIAND Noe <noebriand2003@gmail.com>
2026-05-31 12:23:11 +0200

    debut de reduced

--- domains/reducedProduct.ml
+++ domains/reducedProduct.ml
@@ -3,27 +3,30 @@ open Frontend.AbstractSyntax
 open Congruence
 open Interval
 
-type t = unit
+type t = Congruence.t * Interval.t
 
-let pp fmt x = failwith "wip"
+let pp fmt x = Format.fprintf fmt "(%a, %a)" Congruence.pp (fst x) Interval.pp (snd x)
 
-let top = ()
-let bottom = ()
-let is_bottom _ = failwith "wip"
+let top = (Congruence.top, Interval.top)
+let bottom = (Congruence.bottom, Interval.bottom)
 
-let const c = failwith "wip"
-let rand x y = failwith "wip"
+let const c = (Congruence.const c, Interval.const c)
+let rand x y = (Congruence.rand x y, Interval.rand x y)
 
-let unary x op = failwith "wip"
-let binary x y op = failwith "wip"
+let unary x op = (Congruence.unary (fst x) op, Interval.unary (snd x) op)
+let binary x y op = (Congruence.binary (fst x) (fst y) op, Interval.binary (snd x) (snd y) op )
+let p (c,i) = failwith "wip"
+let is_bottom (c,i) = Congruence.is_bottom c || Interval.is_bottom i
 
-let join x y = failwith "wip"
-let meet x y = failwith "wip"
-let widen x y = failwith "wip"
+let join (c1,i1) (c2,i2) = p (Congruence.join c1 c2, Interval.join i1 i2)
+let meet (c1,i1) (c2,i2) = p (Congruence.meet c1 c2, Interval.meet i1 i2)
+let widen (c1,i1) (c2,i2) = (Congruence.widen c1 c2, Interval.widen i1 i2)
 
-let compare x y = failwith "wip"
-let leq x y = failwith "wip"
+let compare (c1,i1) (c2,i2) op = failwith "wip"
+let leq (c1,i1) (c2,i2) = Congruence.leq c1 c2 && Interval.leq i1 i2
 
-let bwd_unary _ _ _ = failwith "not implemented"
-let bwd_binary _ _ _ = failwith "not implemented"
-let narrow _ _ = failwith "not implemented"
+let bwd_unary x op r = failwith "not implemented"
+
+let bwd_binary x y op r = failwith "not implemented"
+
+let narrow x y = failwith "not implemented"
